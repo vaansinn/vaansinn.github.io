9f892b77a41a1e24cc99827718829fb384c3d8dc
vaansinn <40579465+vaansinn@users.noreply.github.com>
2018-07-01 18:27:52 +0100

    Update earthquakeWDC.js

--- earthquakeWDC.js
+++ earthquakeWDC.js
@@ -27,9 +27,25 @@ myConnector.getSchema = function (schemaCallback) {
     schemaCallback([tableSchema]);
 };
 
-    myConnector.getData = function (table, doneCallback) {
+myConnector.getData = function(table, doneCallback) {
+    $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
+        var feat = resp.features,
+            tableData = [];
 
-    };
+        // Iterate over the JSON object
+        for (var i = 0, len = feat.length; i < len; i++) {
+            tableData.push({
+                "id": feat[i].id,
+                "mag": feat[i].properties.mag,
+                "title": feat[i].properties.title,
+                "location": feat[i].geometry
+            });
+        }
+
+        table.appendRows(tableData);
+        doneCallback();
+    });
+};
 
     tableau.registerConnector(myConnector);
     $(document).ready(function () {
